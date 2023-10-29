efc9a45c38ee87604d62c753f4d61563376539af
zaaaiiimaaa <zaimaahmed101@gmail.com>
2023-10-29 11:25:57 +0600

    delete kore disi

--- demo6 (progga)/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.almasb.fxgl.all;
-
-    opens com.example.demo6 to javafx.fxml;
-    exports com.example.demo6;
-}
